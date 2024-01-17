1e7a063fd5d3e9adee28e28faebfa53a4c611f0b
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-17 17:39:06 +0700

    ✅ adding favico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#bd0023" />
+      <meta name="msapplication-TileColor" content="#b91d47" />
+      <meta name="theme-color" content="#ffffff" />
       <body className={montserrat.className}>{children}</body>
     </html>
   )
