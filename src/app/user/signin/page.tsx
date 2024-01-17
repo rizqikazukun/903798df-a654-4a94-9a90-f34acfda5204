dd8018853c518e67976cd858e1b12e1fbee370da
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-17 22:15:51 +0700

    ✅ adding pages

---
+++ src/app/user/signin/page.tsx
@@ -0,0 +1,8 @@
+'use server'
+import React from 'react'
+
+export default async function Signin(): Promise<JSX.Element> {
+  return (
+    <div>page</div>
+  )
+}
---
+++ src/app/user/signup/page.tsx
@@ -0,0 +1,8 @@
+'use server'
+import React from 'react'
+
+export default async function Signup(): Promise<JSX.Element> {
+  return (
+    <div>page</div>
+  )
+}
