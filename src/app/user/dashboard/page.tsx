aa57405ca68e7948631e3fbd3567040bafcdafb7
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-18 11:45:19 +0700

    🔷 update dashboard page

--- src/app/user/dashboard/page.tsx
+++ src/app/user/dashboard/page.tsx
@@ -1,8 +1,18 @@
 'use server'
 import React from 'react'
+import { Navbar, Footer } from '@/app/_components'
+
+
 
 export default async function Dashboard(): Promise<JSX.Element> {
   return (
-    <div>page</div>
+    <div id="dashboard-user">
+      <div style={{ minHeight: 'calc(100vh - 178px)' }}>
+        <Navbar />
+        <div className='container mx-auto max-w-[1000px] my-10 rounded-lg border shadow-sm bg-white'>
+        </div>
+      </div>
+      <Footer />
+    </div>
   )
 }
