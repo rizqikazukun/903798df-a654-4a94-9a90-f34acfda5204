124fee9a0b0721b37c833d23ad6bc9af5d553365
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-18 11:46:48 +0700

    🔷 update footer element

--- src/app/_components/BasicComponent/Footer.tsx
+++ src/app/_components/BasicComponent/Footer.tsx
@@ -4,14 +4,13 @@ import React from 'react'
 
 export async function Footer(): Promise<JSX.Element> {
     return (
-        <nav>
-            <div className='container mx-auto mt-10 p-10 flex flex-wrap gap-5 max-lg:justify-center lg:justify-between items-center px-10 rounded-t-lg bg-white border max-w-[1200px]'>
-                <img className=" h-14" src="/assets/images/wooz-logo.svg" alt="logo" />
-                <p className='font-medium text-center'>
-                    Wooz is a Test Base Project. Build by Rizqi Pratama. ©2024
-                </p>
-                <img className=" h-14" src="/assets/images/rizqipratama-logo.svg" alt="logo" />
-            </div>
-        </nav>
+        <footer className='container mx-auto p-10 mt-10 flex flex-wrap gap-5 max-lg:justify-center 
+                            lg:justify-between items-center rounded-t-lg bg-white border max-w-[1200px]'>
+            <img className=" h-14" src="/assets/images/wooz-logo.svg" alt="logo" />
+            <p className='font-medium text-center'>
+                Wooz is a Test Base Project. Build by Rizqi Pratama. ©2024
+            </p>
+            <img className=" h-14" src="/assets/images/rizqipratama-logo.svg" alt="logo" />
+        </footer>
     )
 }
