4627a173f7385edaa5970b0bcbc11fdad759dff2
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-18 10:13:59 +0700

    🔷 update signin page

--- src/app/_components/BasicComponent/SigninForm.tsx
+++ src/app/_components/BasicComponent/SigninForm.tsx
@@ -1,14 +1,40 @@
 'use client'
+import { ComponentPassing } from '@/lib/TypeInterface'
+import Link from 'next/link'
 import React from 'react'
 
-export function SigninForm(prop : {BackendURL?: string | undefined}): JSX.Element {
+export function SigninForm(prop: ComponentPassing): JSX.Element {
 
-  const BackendURL: string | undefined = prop.BackendURL || undefined
+  const BackendURL: string | undefined = prop.BeURL || undefined
 
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
-  
+
+
   return (
-    <div>SigninForm</div>
+    <div id='signup-form' className='flex flex-col gap-10 w-full'>
+      <div className='flex flex-col gap-2'>
+        <label htmlFor="email">Email :</label>
+        <input className='p-2 rounded-full border' type="email" id='email' onChange={e => setEmail(e.target.value)} />
+        <label htmlFor="password">Passsword :</label>
+        <input className='p-2 rounded-full border' type="password" id='password' onChange={e => setPassword(e.target.value)} />
+      </div>
+
+      <div className='flex flex-col w-full items-center justify-center gap-2'>
+        <button className='woozify-button border shadow-sm p-2 rounded-full w-full hover:font-bold text-white'>
+            Sign In
+        </button>
+        <p>
+          Don&apos;t Have Account? {' '}
+          <Link href="/user/signup">
+            <span className='font-medium text-red-800'>Sign Up
+            </span>
+          </Link>
+        </p>
+        <p>
+          <Link href="/#">Back to Home</Link>
+        </p>
+      </div>
+    </div>
   )
 }
--- src/app/user/signin/page.tsx
+++ src/app/user/signin/page.tsx
@@ -1,8 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
 'use server'
 import React from 'react'
+import { SigninForm } from '@/app/_components'
+import { BeURL } from '@/lib/Config'
 
 export default async function Signin(): Promise<JSX.Element> {
+
   return (
-    <div>page</div>
+    <div id='signup'>
+      <div className='mx-auto md:my-8 py-28 px-10 max-w-[800px] bg-white border 
+                      shadow-sm rounded-lg flex flex-col justify-center items-center gap-2'>
+        <img src="/assets/images/wooz-logo.svg" alt="logo" className='h-20' />
+        <p className='font-bold text-2xl'>Sign In</p>
+        <SigninForm BeURL={BeURL}/>
+      </div>
+    </div>
   )
 }
