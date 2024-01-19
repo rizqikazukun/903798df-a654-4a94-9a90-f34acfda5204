0fd10beab457deaf05a83b855c16d27a31e77932
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-19 21:39:28 +0700

    🔷 update page register

--- src/app/user/register/page.tsx
+++ src/app/user/register/page.tsx
@@ -3,16 +3,19 @@
 import React from 'react'
 import { RegisterForm } from '@/app/_components'
 import { BeURL } from '@/lib/Config'
+import { BlockIfLogin } from '@/lib/ServerAction'
 
 export default async function Register(): Promise<JSX.Element> {
 
+  await BlockIfLogin()
+
   return (
-    <div id='signup'>
+    <div id='register'>
       <div className='mx-auto md:my-8 py-28 px-10 max-w-[800px] bg-white border 
                       shadow-sm rounded-lg flex flex-col justify-center items-center gap-2'>
         <img src="/assets/images/wooz-logo.svg" alt="logo" className='h-20' />
         <p className='font-bold text-2xl'>Register</p>
-        <RegisterForm BeURL={BeURL}/>
+        <RegisterForm BeURL={BeURL} />
       </div>
     </div>
   )
