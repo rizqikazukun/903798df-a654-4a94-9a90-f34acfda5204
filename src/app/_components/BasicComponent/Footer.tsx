c1904390220a1ee5bb5bb5a99592854b530ff816
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-19 14:33:40 +0700

    🔷 update footer

--- src/app/_components/BasicComponent/Footer.tsx
+++ src/app/_components/BasicComponent/Footer.tsx
@@ -1,16 +1,21 @@
-'use server'
 /* eslint-disable @next/next/no-img-element */
+'use server'
+import Link from 'next/link'
 import React from 'react'
 
 export async function Footer(): Promise<JSX.Element> {
     return (
         <footer className='container mx-auto p-10 mt-10 flex flex-wrap gap-5 max-lg:justify-center 
                             lg:justify-between items-center rounded-t-lg bg-white border max-w-[1200px]'>
-            <img className=" h-14" src="/assets/images/wooz-logo.svg" alt="logo" />
+            <Link href='/#'>
+                <img className=" h-14" src="/assets/images/wooz-logo.svg" alt="logo" />
+            </Link>
             <p className='font-medium text-center'>
                 Wooz is a Test Base Project. Build by Rizqi Pratama. ©2024
             </p>
-            <img className=" h-14" src="/assets/images/rizqipratama-logo.svg" alt="logo" />
+            <Link href='https://portfolio-rizqi-pratama.vercel.app' target='_blank'>
+                <img className=" h-14" src="/assets/images/rizqipratama-logo.svg" alt="logo" />
+            </Link>
         </footer>
     )
 }
