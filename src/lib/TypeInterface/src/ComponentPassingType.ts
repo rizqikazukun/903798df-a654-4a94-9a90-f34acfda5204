de7899055ba2c80899288c1baafe99d6bc6acf27
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-19 22:29:53 +0700

    🔷 update component passing type

--- src/lib/TypeInterface/src/ComponentPassingType.ts
+++ src/lib/TypeInterface/src/ComponentPassingType.ts
@@ -1,5 +1,8 @@
+import { ShortUrlResultType } from ".."
+
 export interface ComponentPassingType { 
     AppURL?: string | undefined 
     BeURL?: string | undefined 
     session_id?: string | undefined
+    data?: ShortUrlResultType | any
 }
