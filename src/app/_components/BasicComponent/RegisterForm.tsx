a054278471f61ad148d1ffc5a9f10b7cc5826f0e
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-19 14:32:10 +0700

    🔷 integrating user register

--- src/app/_components/BasicComponent/RegisterForm.tsx
+++ src/app/_components/BasicComponent/RegisterForm.tsx
@@ -1,17 +1,91 @@
 'use client'
 import { ComponentPassingType } from '@/lib/TypeInterface'
+import axios from 'axios'
 import Link from 'next/link'
 import React from 'react'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { useRouter } from 'next/navigation'
 
 export function RegisterForm(prop: ComponentPassingType): JSX.Element {
-
+  const MySwal = withReactContent(Swal)
   const BackendURL: string | undefined = prop.BeURL || undefined
+  const router = useRouter()
 
   const [first_name, setFirstName] = React.useState('')
   const [last_name, setLastName] = React.useState('')
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
   const [passwordC, setPasswordC] = React.useState('')
+
+  const registerHandler = React.useCallback(async () => {
+    try {
+      if (password !== passwordC) {
+        throw { status: 422, message: 'Password confirmation not match' }
+      }
+
+      await axios({
+        method: 'post',
+        url: `${BackendURL}/auth/register`,
+        data: {
+          first_name,
+          last_name,
+          email,
+          password
+        }
+      })
+
+      MySwal.fire({
+        titleText: 'Register success',
+        timer: 2000,
+        showCancelButton: false,
+        showConfirmButton: false
+      })
+
+      router.replace('/user/login')
+    } catch (error: any) {
+      console.log(error)
+      let message: string = ''
+      let arrayMessage: Array<string> = []
+
+      if (error.status === 422) {
+        message = error.message
+
+        MySwal.fire({
+          titleText: message,
+          showCancelButton: false,
+          showConfirmButton: true,
+        })
+      } else if (error.response.status === 409) {
+        message = error.response.data.message
+
+        MySwal.fire({
+          titleText: message,
+          showCancelButton: false,
+          showConfirmButton: true,
+        })
+      } else if (error.response.status === 400) {
+      
+        arrayMessage = error.response.data.message
+        message += '<div id="custom-message">'
+
+        for (const index in arrayMessage) {
+          message += `<p>${Number(index)+1}. ${arrayMessage[index]}.</p>`
+        }
+
+        message += '</div>'
+
+        MySwal.fire({
+          titleText: 'Register failed please check',
+          html: message,
+          showCancelButton: false,
+          showConfirmButton: true,
+        })
+        
+      }
+    }
+  }, [BackendURL, MySwal, email, first_name, last_name, password, passwordC, router])
+
 
   return (
     <div id='signup-form' className='flex flex-col gap-10 w-full'>
@@ -24,13 +98,14 @@ export function RegisterForm(prop: ComponentPassingType): JSX.Element {
         <input className='p-2 rounded-full border' type="email" id='email' onChange={e => setEmail(e.target.value)} />
         <label htmlFor="password">Passsword :</label>
         <input className='p-2 rounded-full border' type="password" id='password' onChange={e => setPassword(e.target.value)} />
-        <label htmlFor="password">Confirm Password :</label>
-        <input className='p-2 rounded-full border' type="password" id='password' onChange={e => setPasswordC(e.target.value)} />
+        <label htmlFor="password-confirmation">Confirm Password :</label>
+        <input className='p-2 rounded-full border' type="password" id='password-confirmation' onChange={e => setPasswordC(e.target.value)} />
       </div>
 
       <div className='flex flex-col w-full items-center justify-center gap-2'>
-        <button className='woozify-button border shadow-sm p-2 rounded-full w-full hover:font-bold text-white'>
-            Sign Up
+        <button className='woozify-button border shadow-sm p-2 rounded-full w-full hover:font-bold text-white'
+          onClick={registerHandler}>
+          Register
         </button>
         <p>
           Have an account? {' '}
