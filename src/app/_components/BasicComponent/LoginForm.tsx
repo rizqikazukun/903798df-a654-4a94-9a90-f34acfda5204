f92e24cc2dffec10bbc098a42423f12ef83dc02c
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-19 21:37:54 +0700

    🔷 integrating login

--- src/app/_components/BasicComponent/LoginForm.tsx
+++ src/app/_components/BasicComponent/LoginForm.tsx
@@ -2,13 +2,83 @@
 import { ComponentPassingType } from '@/lib/TypeInterface'
 import Link from 'next/link'
 import React from 'react'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
+import { setCookie } from 'cookies-next'
 
 export function LoginForm(prop: ComponentPassingType): JSX.Element {
-
+  const MySwal = withReactContent(Swal)
   const BackendURL: string | undefined = prop.BeURL || undefined
+  const router = useRouter()
 
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
+
+  const loginHandler = React.useCallback(async () => {
+    try {
+      const login = await axios({
+        method: 'post',
+        url: `${BackendURL}/auth/login`,
+        data: {
+          email,
+          password
+        }
+      })
+
+      const currentTime : number = new Date().getTime()
+      const accessExp : number = 15 * 60 * 1000
+      const refreshExp : number = 7 * 24 * 60 * 60 * 10000 
+      const profileExp : number = 365 * 24 * 60 * 60 * 10000 
+
+      setCookie('profile', JSON.stringify(login.data.data), { expires: new Date(currentTime+profileExp)})
+      setCookie('accessToken', `Bearer ${login.data.accessToken}`, { expires: new Date(currentTime+accessExp)})
+      setCookie('refreshToken', `Bearer ${login.data.refreshToken}`, { expires: new Date(currentTime+refreshExp)})
+
+      MySwal.fire({
+        titleText: 'Login success',
+        timer: 2000,
+        showCancelButton: false,
+        showConfirmButton: false
+      })
+
+      router.replace('/')
+    } catch (error: any) {
+      console.log(error)
+      let message: string = ''
+      let arrayMessage: Array<string> = []
+
+      if (error.response.status === 400 || error.response.status === 404) {
+        if (typeof error.response.data.message === 'string') {
+          message += error.response.data.message
+
+          MySwal.fire({
+            titleText: message,
+            showCancelButton: false,
+            showConfirmButton: true,
+          })
+        } else {
+          arrayMessage = error.response.data.message
+          message += '<div id="custom-message">'
+
+          for (const index in arrayMessage) {
+            message += `<p>${Number(index) + 1}. ${arrayMessage[index]}.</p>`
+          }
+
+          message += '</div>'
+
+          MySwal.fire({
+            titleText: 'Login failed please check',
+            html: message,
+            showCancelButton: false,
+            showConfirmButton: true,
+          })
+        }
+
+      }
+    }
+  }, [BackendURL, MySwal, email, password, router])
 
 
   return (
@@ -21,7 +91,8 @@ export function LoginForm(prop: ComponentPassingType): JSX.Element {
       </div>
 
       <div className='flex flex-col w-full items-center justify-center gap-2'>
-        <button className='woozify-button border shadow-sm p-2 rounded-full w-full hover:font-bold text-white'>
+        <button className='woozify-button border shadow-sm p-2 rounded-full w-full hover:font-bold text-white'
+          onClick={loginHandler}>
           Login
         </button>
         <p>
--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -3,16 +3,20 @@
 import React from 'react'
 import { LoginForm } from '@/app/_components'
 import { BeURL } from '@/lib/Config'
+import { BlockIfLogin, BlockIfNotLogin } from '@/lib/ServerAction'
+
 
 export default async function Signin(): Promise<JSX.Element> {
+  
+  await BlockIfLogin()
 
   return (
-    <div id='signup'>
+    <div id='login'>
       <div className='mx-auto md:my-8 py-28 px-10 max-w-[800px] bg-white border 
                       shadow-sm rounded-lg flex flex-col justify-center items-center gap-2'>
         <img src="/assets/images/wooz-logo.svg" alt="logo" className='h-20' />
         <p className='font-bold text-2xl'>Login</p>
-        <LoginForm BeURL={BeURL}/>
+        <LoginForm BeURL={BeURL} />
       </div>
     </div>
   )
